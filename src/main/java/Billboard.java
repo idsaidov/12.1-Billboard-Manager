b10e1ffd6ff9d0696a0cf5945cd562a0db39250b
idsaidov <id.saidov@yandex.ru>
2022-09-10 15:24:33 +0300

    Performing DZ using Lomboc

---
+++ src/main/java/Billboard.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+
+public class Billboard {
+
+    private int id;
+    private String name;
+    private String genre;
+    private String Url;
+    private boolean Announcement;
+
+}
---
+++ src/main/java/BillboardManager.java
@@ -0,0 +1,40 @@
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Arrays;
+
+@NoArgsConstructor
+@Data
+
+public class BillboardManager {
+    private int limit = 10;
+    private Billboard[] billboards = new Billboard[0];
+
+    public BillboardManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addMovie(Billboard movie) {
+        int length = billboards.length + 1;
+        Billboard[] tmp = Arrays.copyOf(billboards, length);
+        tmp[length - 1] = movie;
+        billboards = tmp;
+    }
+
+    public Billboard[] showAllMovies() {
+        Billboard[] result = new Billboard[billboards.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = billboards[i];
+        }
+        return result;
+    }
+
+    public Billboard[] showLastLimitedNumberOfMovies() {
+        int resultLength = (this.limit > billboards.length) ? billboards.length : this.limit;
+        Billboard[] result = new Billboard[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = billboards.length - i - 1;
+            result[i] = billboards[index];
+        }
+        return result;
+    }
+}
